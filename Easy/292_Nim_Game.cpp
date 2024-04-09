9e1bcde660fea7d9c1e1b8254bd728524da8128b
NullCipherr <blendsteal@gmail.com>
2024-04-09 14:01:01 -0300

    add Nim_Game.cpp in easy.

---
+++ Easy/292_Nim_Game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    // Verifica se n é um múltiplo de 4. Se n não for um múltiplo de 4, você pode ganhar o jogo, então a função        retorna true. Caso contrário, você perderá o jogo, então a função retorna false.
+    bool canWinNim(int n) {
+        return n % 4 != 0 ;
+    }
+};
