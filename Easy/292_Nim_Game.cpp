ffa840e4e6461027ba068566f522b2a972bc9c98
Andrei Costa <141879308+NullCipherr@users.noreply.github.com>
2024-04-09 14:01:35 -0300

    Update 292_Nim_Game.cpp

--- Easy/292_Nim_Game.cpp
+++ Easy/292_Nim_Game.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Verifica se n é um múltiplo de 4. Se n não for um múltiplo de 4, você pode ganhar o jogo, então a função        retorna true. Caso contrário, você perderá o jogo, então a função retorna false.
+    // Verifica se n é um múltiplo de 4. Se n não for um múltiplo de 4, você pode ganhar o jogo, então a função retorna true. Caso contrário, você perderá o jogo, então a função retorna false.
     bool canWinNim(int n) {
         return n % 4 != 0 ;
     }
